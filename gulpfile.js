ac8ea6565b9c558390fd8770b8da87996b0648d7
Declan Van Welie <dvanwelie@sva.edu>
2017-03-15 16:08:13 -0400

    Fixed: relative paths to assets

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var config = {
 }
 
 var paths = {
-    bundleResources: './src/ContentBundle/Resources',
+    bundleResources: './resources',
     destination: './web/dist',
     vendor: './src/ContentBundle/Resources/vendor',
     assets_dest: '../../assets',
@@ -37,7 +37,7 @@ gulp.task('bootcardsStyles', function () {
     .pipe(concat('bootcards.css'))
     // .pipe((config.dev)?  utils.noop() : minify())
     .pipe(gulp.dest(paths.destination + '/css'))
-    .pipe(livereload());
+    // .pipe(livereload());
 })
 
 gulp.task('styles', function () {
@@ -61,14 +61,14 @@ gulp.task('styles', function () {
             .pipe(concat('app.css'))
             // .pipe((config.dev)?  utils.noop() : minify())
             .pipe(gulp.dest(paths.destination + '/css'))
-            .pipe(livereload());
+            // .pipe(livereload());
 
     return gulp.src(paths.bundleResources + '/sass/overrides.sass')
             .pipe(sass())    
             .on('error', function (err) { console.log(err.message); })
             // .pipe((config.dev)?  utils.noop() : minify())
             .pipe(gulp.dest(paths.destination + '/css'))
-            .pipe(livereload());
+            // .pipe(livereload());
 
 });
 
@@ -93,7 +93,7 @@ gulp.task('scripts', function() {
         .pipe(concat('app.js'))
         .pipe((config.dev)?  utils.noop() : uglify())
         .pipe(gulp.dest(paths.destination + '/js'))
-        .pipe(livereload());;
+        // .pipe(livereload());;
 });
 
 
@@ -102,7 +102,7 @@ gulp.task('pages', function(){
         paths.bundleResources + '/views/**/*.twig',
         paths.bundleResources + '../Content/**/*.md'
         ])
-        .pipe(livereload());
+        // .pipe(livereload());
 });
 gulp.task('fonts', function(){
     return gulp.src([
@@ -111,22 +111,22 @@ gulp.task('fonts', function(){
             paths.assets_src  + '/fonts/**/*'
         ])
         .pipe(gulp.dest(paths.assets_dest + '/fonts'))
-        .pipe(livereload());
+        // .pipe(livereload());
 });
-gulp.task('img', function(){
-    return gulp.src([
-            paths.assets_src + '/img/**/*' 
-        ], {base: paths.assets_src + '/img'})
-        .pipe(gulp.dest(paths.assets_dest + '/img'))
-        .pipe(livereload());
-});
+// gulp.task('img', function(){
+//     return gulp.src([
+//             paths.assets_src + '/img/**/*'
+//         ], {base: paths.assets_src + '/img'})
+//         .pipe(gulp.dest(paths.assets_dest + '/img'))
+//         .pipe(livereload());
+// });
 
 
 gulp.task('watch', function(){
     config.dev = true;
-    livereload.listen({
-        port: config.livereloadPort,
-    });
+    // livereload.listen({
+    //     port: config.livereloadPort,
+    // });
     gulp.watch(paths.bundleResources + '/sass/**/*.sass', ['styles']);
     gulp.watch([
         paths.bundleResources + '/js/*.js',
@@ -143,12 +143,14 @@ gulp.task('watch', function(){
 
 gulp.task('dev',function(){
 	config.dev = true;
-	gulp.start(['styles', 'bootcardsStyles', 'scripts','img','fonts'])
+	// gulp.start(['styles', 'bootcardsStyles', 'scripts','img','fonts'])
+	gulp.start(['styles', 'bootcardsStyles', 'scripts','fonts'])
 })
 
 gulp.task('prod',function(){
 	config.dev = false;
-	gulp.start(['styles', 'bootcardsStyles', 'scripts','img','fonts'])
+	// gulp.start(['styles', 'bootcardsStyles', 'scripts','img','fonts'])
+	gulp.start(['styles', 'bootcardsStyles', 'scripts','fonts'])
 })
 
 gulp.task('default', function() {
